4b0939437fa578383b1a0910c1984e4844db1e00
akashdutt <akash.dutt@successive.tech>
2019-01-17 12:11:26 +0530

    fix:diamond.js

--- extra/pattern/diamond.js
+++ extra/pattern/diamond.js
@@ -4,18 +4,12 @@ function diamond(n){
     for(let i=1;i<=n;i++){
     for(let j=1;j<=n-i;j++){
         pattern=pattern+' '
-        
-        
     }
     for(let j=1;j<=2*i-1;j++){
         pattern= pattern+'*';
-        
-
     }
-
     console.log(pattern)
     pattern=''
-    
     }
     for( let k=1;k<=n;k++){
         for(let j=1;j<=k;j++){
