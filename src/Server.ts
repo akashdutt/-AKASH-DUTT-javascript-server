64a23bbb2a3c555128e0a9c19adf8c7d35d3d416
akashdutt <akash.dutt@successive.tech>
2019-01-23 17:07:04 +0530

    added:errorHandler.ts,index.ts,notFoundRoute.ts , modified:package.json,Server.ts

--- src/Server.ts
+++ src/Server.ts
@@ -1,10 +1,13 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
+import { errorHandler, notFoundRoute } from "./libs/routes/index";
 class Server {
 	public app: express.Express;
 	constructor(private config) {
 		this.app = express();
 	}
 	public bootstrap() {
+		this.initBodyParser();
 		this.setupRoutes();
 		return this;
 	}
@@ -12,6 +15,13 @@ class Server {
 		this.app.use("/health-check", (req, res) => {
 			res.send("I am OK");
 		});
+		this.app.use(notFoundRoute);
+		this.app.use(errorHandler);
+	}
+	private initBodyParser() {
+		const { app } = this;
+		app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded({ extended: false }));
 	}
 	public run() {
 		const {
